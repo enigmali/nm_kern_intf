193a033a5aa88dab7f37d31487ade50e82a20390
Enigma Li <lizhilong.enigma@gmail.com>
2016-09-19 10:32:17 -0400

    add basic data types of netmap

--- nm_kern.h
+++ nm_kern.h
@@ -5,6 +5,7 @@
 #define IF_NAME_SIZE		44
 #define NM_ERRBUF_SIZE		512
 
+#define NM_CACHE_ALIGN		128
 
 struct netmap_if {
 	char			ni_name[IF_NAME_SIZE];
@@ -22,6 +23,58 @@ struct netmap_if {
 	const ssize_t	rings_ofs[0];
 };
 
+struct nmreq {};
+
+struct timeval {
+	uint64_t	tv_sec;
+	uint64_t	tv_usec;
+};
+
+struct netmap_slot {
+	uint32_t	buf_idx;
+	uint16_t	len;
+	uint16_t	flags;
+	uint64_t	ptr;
+};
+
+struct nm_pkthdr {
+	struct timeval		ts;
+	uint32_t			caplen;
+	uint32_t			len;
+	uint64_t			flags;
+
+	// #define NM_MORE_PKTS		1
+	
+	struct nm_desc		*d;
+	struct netmap_slot	*slot;
+	uint8_t				*buf;
+};
+
+struct netmap_ring {
+	const int64_t		buf_ofs;
+	const uint32_t		num_slots;
+	const uint32_t		nr_buf_size;
+	const uint16_t		ringid;
+	const uint16_t		dir;
+
+	uint32_t			head;
+	uint32_t			cur;
+	uint32_t			tail;
+	uint32_t			flags;
+
+	struct timeval		ts;
+
+	uint8_t	__attribute__((__aligned__(NM_CACHE_ALIGN)))	sem[128];
+
+	struct netmap_slot	slot[0];
+	
+};
+
+struct nm_stat {
+	uint32_t	ps_recv;
+	uint32_t	ps_drop;
+	uint32_t	ps_ifdrop;
+};
 
 struct nm_desc {
 	struct nm_desc		*self;
